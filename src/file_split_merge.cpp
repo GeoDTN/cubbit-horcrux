78b3784941effd3cf647617cfe8a0e098fd21680
Tadewos Somano <tadewos.ewalo@sparklingRocks.it>
2024-02-02 20:13:28 +0100

    use pure c++ for local copy

--- src/file_split_merge.cpp
+++ src/file_split_merge.cpp
@@ -1,25 +1,30 @@
 #include <fstream>
 #include <cstdio>
 #include <cstdlib>
+#include <utility>
 #include <set>
 
 #include "file_split_merge.h"
 #include "file.h"
 #include "customexceptions.h"
 
+namespace fs            = std::filesystem;
+const auto copy_options = fs::copy_options::recursive|fs::copy_options::update_existing;
+auto permissions        = fs::perms::owner_all|fs::perms::group_all;
+
 void fileSplitMerge::split(size_t splitNumber,std::string& inputfilePath, std::string& outputPath ) {
-  namespace fs = std::filesystem;
-  secure_string file_to_string = pathStringHandler::filePathToString(inputfilePath);
+  std::string file_to_string = pathStringHandler::filePathToString(inputfilePath);
   size_t buffer_size = (file_to_string.length()/splitNumber)+1;
   std::ifstream ifs(inputfilePath, std::ios::in | std::ios::binary);
 
   char buf[buffer_size];
   size_t file_number = 0;
-  system("mkdir tmp");
-  while (ifs.read(buf, sizeof(buf)) || ifs.gcount()) 
+
+  fs::create_directory("tmp");
+  fs::permissions("tmp", permissions);
+  while (ifs.read(buf, (buffer_size-1)) || ifs.gcount()) 
   {
     // use what was read in.
-
     // gcount() is the number of bytes actually read
     std::string chunk(buf, ifs.gcount());
     auto file_name = std::string(fs::path(inputfilePath).filename());
@@ -29,34 +34,33 @@ void fileSplitMerge::split(size_t splitNumber,std::string& inputfilePath, std::s
       std::ofstream(splitted_component)<< chunk << '\n';
     }
 
-    //fs::copy(fs::path(split_component),fs::path(outputPath),fs::copy_options::overwrite_existing);
-    fs::copy(fs::path(splitted_component),fs::path(std::string_view("tmp/")),fs::copy_options::overwrite_existing);
-    std::filesystem::remove(splitted_component);
-   ++file_number;
+    fs::copy(splitted_component, "tmp", copy_options);
+    fs::remove_all(splitted_component);
+    ++file_number;
 
   }
-  system((std::string("scp -r tmp/* ")+outputPath).c_str());
-  system("sudo rm -rf tmp");
+  fs::copy("tmp", outputPath, copy_options);
+  fs::remove_all("tmp");
 }
 
 void fileSplitMerge::merge(std::string& filesPath,
                            std::string& outPutFilePath) {
-    std::cout << "merge called:  " << std::endl;
-  namespace fs = std::filesystem;
-  std::set<fs::path> files_to_merge;
-  system("mkdir tmp");
-  system((std::string("scp -r ")+filesPath+std::string("/*  tmp")).c_str());
-  fs::path encrypted_files_path = fs::path(std::string("tmp"));
-  std::ofstream ofile;
-  std::cout<<(outPutFilePath+std::string("/retrieved.txt"))<<std::endl;
-  ofile.open ((outPutFilePath+std::string("/retrieved.txt")), std::ofstream::out | std::ofstream::app);
- 
+  std::cout << "merge called:  " << std::endl;
+  fs::create_directory("tmp");
+  fs::permissions("tmp", permissions);
+  fs::copy(filesPath, "tmp", copy_options);
+
+  fs::path encrypted_files_path = fs::path("tmp");
+  std::ofstream ofile((outPutFilePath+"/retrieved.txt"s), std::ofstream::out | std::ofstream::app);
+  std::set<std::string> file_list{};
   ofile.unsetf(std::ios_base::skipws);
   for (const auto& file : fs::directory_iterator{encrypted_files_path}) {
-   std::cout<<std::string(((fs::path)file));
-
-    std::ifstream ifile{};//((fs::path)file)/*, std::ios::in | std::ios::binary)*/;
-    ifile.open(std::string((fs::path)file), std::ios::in | std::ios::binary);
+   file_list.insert(std::string(((fs::path)file)));
+  }
+  for(const auto& file:file_list)
+  {
+    std::cout<<std::string(((fs::path)file))<<std::endl;
+    std::ifstream ifile(std::string((fs::path)file), std::ios::in | std::ios::binary);
     if (!ifile.is_open())
     {
       throw invalid_file("File opening error: ");
@@ -65,16 +69,13 @@ void fileSplitMerge::merge(std::string& filesPath,
     std::string str{};
     while(std::getline (ifile,str))
     {
-    std::cout<<str<<"\n";
     str +="\n";
-    ofile<<str;
-  
-    }
-   
-    //std::filesystem::remove(std::string(file));
+    ofile<<std::move(str);
+    str.clear();
+    }  
+    fs::remove_all(file);
   }
-
-  //std::flush(ofile);
-  system("sudo rm -rf tmp");
-
+  ofile.close();std::flush(ofile);
+  fs::remove_all(fs::path("tmp"));
+  std::cout<<"safely reached end of merge"<<std::endl;
 }
